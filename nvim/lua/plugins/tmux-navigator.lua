4df0873d19e97d7c9bc27e8638c169b065f3fa3f
Sridhar Vemula <thewarrior.316@gmail.com>
2026-09-23 12:02:57 -0700

    add Ctrl-Shift-arrows alongside Ctrl-hjkl for pane navigation

--- nvim/lua/plugins/tmux-navigator.lua
+++ nvim/lua/plugins/tmux-navigator.lua
@@ -15,5 +15,12 @@ return {
 		{ "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Window/pane up" },
 		{ "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Window/pane right" },
 		{ "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Previous window/pane" },
+		-- Same moves on Ctrl-Shift-arrows. Plain Ctrl-arrows are not used: they
+		-- are macOS Spaces/Mission Control shortcuts, and Vim's own back/forward
+		-- a word.
+		{ "<C-S-Left>", "<cmd>TmuxNavigateLeft<cr>", desc = "Window/pane left" },
+		{ "<C-S-Down>", "<cmd>TmuxNavigateDown<cr>", desc = "Window/pane down" },
+		{ "<C-S-Up>", "<cmd>TmuxNavigateUp<cr>", desc = "Window/pane up" },
+		{ "<C-S-Right>", "<cmd>TmuxNavigateRight<cr>", desc = "Window/pane right" },
 	},
 }
